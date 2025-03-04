e284d0c3edaa0d021f72b12df6d1d428a2c18043
feffe1702 <fpm.asd@gmail.com>
2025-03-04 10:01:10 +0100

    style: enhance header navigation with new styles and elements

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,18 @@
 <header>
-    <div class="container">
-        <img src="{{ asset('img/dc-logo.png') }}" alt="DC Comics Logo">
-        <nav>
-            <ul>
-                
+    <div class="container d-flex justify-content-between align-items-center">
+        <img src="{{ asset('img/dc-logo.png') }}" alt="DC Comics Logo" height="100">
+        <nav class="h-100">
+            <ul class="d-flex list-unstyled h-100">
+                <li class="mx-2 fw-bold nav_element py-5">CHARACTERS</li>
+                <li class="mx-2 fw-bold nav_element py-5">COMICS</li>
+                <li class="mx-2 fw-bold nav_element py-5">MOVIES</li>
+                <li class="mx-2 fw-bold nav_element py-5">TV</li>
+                <li class="mx-2 fw-bold nav_element py-5">GAMES</li>
+                <li class="mx-2 fw-bold nav_element py-5">COLLECTIBLES</li>
+                <li class="mx-2 fw-bold nav_element py-5">VIDEOS</li>
+                <li class="mx-2 fw-bold nav_element py-5">FANS</li>
+                <li class="mx-2 fw-bold nav_element py-5">NEWS</li>
+                <li class="mx-2 fw-bold nav_element py-5">SHOP</li>
             </ul>
         </nav>
     </div>
