858e3e37b277917bf28abf3b3afac5950a1943ad
feffe1702 <fpm.asd@gmail.com>
2025-03-04 09:35:09 +0100

    import Bootstrap SCSS and include JavaScript file in layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    @vite(['resources/sass/app.scss'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     @include('partials.header')
