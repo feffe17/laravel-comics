80eb7dc4c1141189ca063eb0baba32c55515a74b
feffe1702 <fpm.asd@gmail.com>
2025-03-05 12:02:07 +0100

    feat: update footer layout with new sections and background image

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,49 @@
 <footer>
-    <p>© 2025 Comics Store</p>
+    <div class="link_section">
+        <div class="container d-flex position-relative">
+            <div class="col-6 d-flex flex-wrap">
+                <div class="list d-flex flex-column mx-2 px-2 fs-5 text-secondary">
+                        <h3 class="py-2 list-title text-light">DC COMICS</h3>
+                        <div class="list-item">Character</div>
+                        <div class="list-item">Comics</div>
+                        <div class="list-item">Movies</div>
+                        <div class="list-item">TV</div>
+                        <div class="list-item">Games</div>
+                        <div class="list-item">Videos</div>
+                        <div class="list-item">News</div>
+                        <h3 class="py-2 list-title text-light">SHOP</h3>
+                        <div class="list-item">Shop DC</div>
+                        <div class="list-item">Shop DC Collectibles</div>
+                </div>
+                <div class="list d-flex flex-column mx-2 px-2 fs-5 text-secondary">
+                    <h3 class="py-2 list-title text-light">DC</h3>
+                    <div class="list-item">Terms Of Use</div>
+                    <div class="list-item">Privacy policy (New)</div>
+                    <div class="list-item">Ad Choices</div>
+                    <div class="list-item">Advertising</div>
+                    <div class="list-item">Jobs</div>
+                    <div class="list-item">Subscriptions</div>
+                    <div class="list-item">Talent Workshops</div>
+                    <div class="list-item">CPSC Certificates</div>
+                    <div class="list-item">Ratings</div>
+                    <div class="list-item">Shop Help</div>
+                    <div class="list-item">Contact Us</div>
+                </div>
+                <div class="list d-flex flex-column mx-2 px-2 fs-5 text-secondary">
+                    <h3 class="py-2 list-title text-light">SITES</h3>
+                    <div class="list-item">DC</div>
+                    <div class="list-item">MAD Magazine</div>
+                    <div class="list-item">DC Kids</div>
+                    <div class="list-item">DC Universe</div>
+                    <div class="list-item">DC Power Visa</div>
+                </div>
+                {{-- <div class="list d-flex flex-column mx-2 px-2 fs-5 text-secondary">
+                    <h3 class="py-2 list-title text-light">SHOP</h3>
+                    <div class="list-item">Shop DC</div>
+                    <div class="list-item">Shop DC Collectibles</div>
+                </div> --}}
+            </div>
+            <div class="col-6 logo-bg ratio1_1" style="background-image: url('img/dc-logo-bg.png')"></div>
+        </div>
+    </div>
 </footer>
